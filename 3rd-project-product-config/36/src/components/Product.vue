807eda4967697ce93e984a1c016a89cfb0925123
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-31 19:16:30 +0200

    extract option group to component

--- 3rd-project-product-config/36/src/components/Product.vue
+++ 3rd-project-product-config/36/src/components/Product.vue
@@ -4,21 +4,15 @@
       <div class="product__info">
         <img class="product__image" src="../../static/laptop.jpg" alt />
         <h1 class="product__name">Laptop 15&quot;</h1>
-        <p class="product__desc">Ultra cienki, wydajny laptop z ekranem o wysokiej rozdzielczości.</p>
+        <p class="product__desc">
+          Ultra cienki, wydajny laptop z ekranem o wysokiej rozdzielczości.
+        </p>
       </div>
 
       <div class="features">
         <h2 class="features__heading">Skonfiguruj urządzenie</h2>
 
-        <div class="option">
-          <h3 class="option__label">Procesor</h3>
-
-          <div class="option__group">
-            <button class="btn btn-primary option__button">i7 2.5 GHz</button>
-            <button class="btn option__button" data-tooltip="+400 PLN">i7 2.8 GHz</button>
-            <button class="btn option__button" data-tooltip="+800 PLN">i7 3.2 GHz</button>
-          </div>
-        </div>
+        <OptionGroup />
 
         <div class="option">
           <h3 class="option__label">Pamięć RAM</h3>
@@ -48,23 +42,35 @@
       <div class="clearfix"></div>
 
       <div class="order">
-        <button class="btn btn-lg btn-success float-right">Złóż zamówienie</button>
+        <button class="btn btn-lg btn-success float-right">
+          Złóż zamówienie
+        </button>
       </div>
     </template>
 
-    <div class="toast toast-success" v-else>Twoje zamówienie zostało przyjęte. Dziękujemy!</div>
+    <div class="toast toast-success" v-else>
+      Twoje zamówienie zostało przyjęte. Dziękujemy!
+    </div>
   </div>
 </template>
 
 <script>
+import OptionGroup from "./OptionGroup";
+
 export default {
   name: "Product",
   props: ["product"],
   computed: {
     finished() {
       return this.$store.state.finished;
-    }
-  }
+    },
+    price() {
+      return this.$store.getters.price;
+    },
+  },
+  components: {
+    OptionGroup,
+  },
 };
 </script>
 
